28403d6c930e395b57dfbfa515eb5a56a0695a90
Fabricio Tavares de Oliveira <fabriciotavareso@gmail.com>
2012-11-22 12:15:31 -0200

    Update test node

--- test-node.js
+++ test-node.js
@@ -2,5 +2,5 @@ var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Teste node\n');
-}).listen(80, 'http://75.101.139.176/');
-console.log('Server running at http://http://75.101.139.176/:80/');
+}).listen(80);
+console.log('Server running at port 80');
